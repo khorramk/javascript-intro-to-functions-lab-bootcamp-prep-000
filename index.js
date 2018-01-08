c19a9d8e9bc11d0532e7267d0c8f5e35f690a286
Khorram Khondkar <khorramk.kbsk@gmail.com>
2018-01-08 23:05:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,4 +19,8 @@ function sayHiToGrandma(string){
   if(string.toLowerCase() === string){
     return "I can't hear you!";
   }
+  
+  if (string.toUpperCase() === string){
+    return "Yes Indeed!";
+  }
 }
